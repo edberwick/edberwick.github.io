ede363f353d097a2aef0607725dd820beb1142c2
edberwick <52750490+edberwick@users.noreply.github.com>
2025-05-01 20:32:57 +0100

    update init.js

--- assets/js/photoswipe-init.js
+++ assets/js/photoswipe-init.js
@@ -1,9 +1,12 @@
-import PhotoSwipeLightbox from '/assets/photoswipe/photoswipe-lightbox.esm.min.js';
+import PhotoSwipeLightbox from '/assets/photoswipe/photoswipe-lightbox.esm.js';
 
 const lightbox = new PhotoSwipeLightbox({
   gallery: '.gallery',
   children: 'a',
-  pswpModule: () => import('/assets/photoswipe/photoswipe.esm.min.js'),
+  pswpModule: () => import('/assets/photoswipe/photoswipe.esm.js'),
+  showHideAnimationType: 'zoom',
+  preload: [1, 3],
+  bgOpacity: 0.85
 });
 
 lightbox.init();
